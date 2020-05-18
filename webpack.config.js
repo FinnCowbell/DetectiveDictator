d24f27eb191e932a5b58adac347687befebb82bf
Finn Navin <Thomasfnavin@gmail.com>
2020-05-18 03:19:59 -0400

    Added Style Elements

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,19 @@ const config = {
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
     ]
   },
   resolve: {
