1987dfd3cca066d48d2bc1b5db9ee0a03c8aac0d
Finn Navin <Thomasfnavin@gmail.com>
2020-05-27 22:06:49 -0400

    More socketURL changes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ class App extends React.Component{
   constructor(props){
     super(props)
     let socket;
+    this.socketURL = this.props.socketURL || "";
     if(this.props.lobbyID){
-      socket = io.connect((this.props.socketURL || "") + `/${this.props.lobbyID.toLowerCase()}`);
+      socket = io.connect(this.socketURL + `/${this.props.lobbyID.toLowerCase()}`);
     } else {
-      socket = io.connect((this.props.socketURL|| "") + `/menu`);
+      socket = io.connect(this.socketURL + `/menu`);
     }
     this.state={
       lobbyID: this.props.lobbyID || null,
@@ -27,7 +28,7 @@ class App extends React.Component{
     this.state.socket.close();
     this.setState({
       lobbyID: newID || null,
-      socket: io.connect(this.props.socketURL + (newID ? `/${newID.toLowerCase()}` : "/menu"))
+      socket: io.connect(this.socketURL + (newID ? `/${newID.toLowerCase()}` : "/menu"))
     });
   }
 
