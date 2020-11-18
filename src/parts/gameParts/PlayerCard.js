3969ff87f36f0a1fffaa8fac3151e9d67b4f6c2c
Finn Navin <Thomasfnavin@gmail.com>
2020-11-17 22:55:37 -0500

    Reverted trump

--- src/parts/gameParts/PlayerCard.js
+++ src/parts/gameParts/PlayerCard.js
@@ -8,12 +8,11 @@ import liberal5 from "../../media/player-cards/liberal5.png";
 import liberal6 from "../../media/player-cards/liberal6.png";
 let liberalCards = [liberal1, liberal2, liberal3, liberal4, liberal5, liberal6];
 const N_LIBERAL_CARDS = liberalCards.length;
-import fascist1 from "../../media/player-cards/red1.png";
-import fascist2 from "../../media/player-cards/red2.png";
-import fascist3 from "../../media/player-cards/red3.png";
-import fascist4 from "../../media/player-cards/red4.png";
-import hitler from "../../media/player-cards/trump.png";
-let fascistCards = [fascist1, fascist2, fascist3, fascist4];
+import fascist1 from "../../media/player-cards/fascist1.png";
+import fascist2 from "../../media/player-cards/fascist2.png";
+import fascist3 from "../../media/player-cards/fascist3.png";
+import hitler from "../../media/player-cards/hitler.png";
+let fascistCards = [fascist1, fascist2, fascist3];
 const N_FASCIST_CARDS = fascistCards.length;
 
 
