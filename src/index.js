a9d0b1bcc6ab3fb6b17b5db9b6eddda1a7d6139b
FinnCowbell <thomasfnavin@gmail.com>
2021-12-28 19:52:04 -0500

    Removing all env vars

--- src/index.js
+++ src/index.js
@@ -5,11 +5,5 @@ import "./styles.scss";
 /*DD_SERVER and DD_PORT are environmental variables given during compilation.
   They are used if the front-end is being served as static files, 
   and the backend is hosted on some other server. */
-const DD_SERVER = process.env.DD_SERVER
-const DD_PORT = process.env.DD_PORT
-let isHeroku = process.env.PORT != ""
 let socketURL = "";
-if (!isHeroku) {
-  socketURL = `${DD_SERVER}:${DD_PORT}`;
-}
 ReactDOM.render(<App socketURL={socketURL} />, document.getElementById("root"));
